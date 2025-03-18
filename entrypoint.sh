de76e3c642730e592e8347596c09d59d0e7f0c1e
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2025-03-18 09:38:48 -0400

    refactor: pipe envsubst output to curl instead of using tmp file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 
-tmpfile=$(mktemp)
-envsubst < "${NIMBUS_PATH}" > "${tmpfile}"
-
-curl --location "${NIMBUS_SERVER}/deploy" \
---header "X-Api-Key: ${NIMBUS_API_KEY}" \
---form "file=@${tmpfile}"
+envsubst < "${NIMBUS_PATH}" | curl --location "${NIMBUS_SERVER}/deploy" \
+    --header "X-Api-Key: ${NIMBUS_API_KEY}" \
+    --form "file=@-"
 
 echo "service-urls=[]" >> $GITHUB_OUTPUT
-rm -f "${tmpfile}"
