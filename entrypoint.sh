3d4bdf62715b2a806f93b1ec096e98d7f744907f
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2025-03-17 22:59:13 -0400

    fix: pipe envsubst output to a temporary file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+tmpfile=$(mktemp)
+envsubst < "${NIMBUS_PATH}" > "${tmpfile}"
+
 curl --location "${NIMBUS_SERVER}/deploy" \
 --header "X-Api-Key: ${NIMBUS_API_KEY}" \
---form "file=@(envsubst < ${NIMBUS_PATH})"
+--form "file=@${tmpfile}"
 
 echo "service-urls=[]" >> $GITHUB_OUTPUT
+rm -f "${tmpfile}"
